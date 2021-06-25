28a27cca32035f783a32b56f1493c6660c47b724
Marcus <Marcus.Ekstrom@devgroup.se>
2021-06-25 16:19:53 +0200

    disable x-powered-by manually

--- myApp.js
+++ myApp.js
@@ -2,7 +2,8 @@ const express = require('express');
 const helmet = require("helmet")
 
 const app = express();
-app.use(helmet.hidePoweredBy());
+app.use(helmet());
+app.disable("x-powered-by")
 
 
 
