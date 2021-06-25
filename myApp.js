fec348ea7cc18915882c9d0eaaa05824a2516d28
Marcus <Marcus.Ekstrom@devgroup.se>
2021-06-25 18:23:32 +0200

    Add helmet repo

---
+++ myApp.js
@@ -0,0 +1,63 @@
+const express = require('express');
+const helmet = require("helmet")
+
+const app = express();
+app.use(helmet())
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+module.exports = app;
+const api = require('./server.js');
+app.use(express.static('public'));
+app.disable('strict-transport-security');
+app.use('/_api', api);
+app.get("/", function (request, response) {
+  response.sendFile(__dirname + '/views/index.html');
+});
+let port = process.env.PORT || 3000;
+app.listen(port, () => {
+  console.log(`Your app is listening on port ${port}`);
+});
