4ce1dba65506d01e778f0c83f9ad08930275a86b
Marcus <Marcus.Ekstrom@devgroup.se>
2021-06-25 17:01:32 +0200

    test

--- myApp.js
+++ myApp.js
@@ -2,8 +2,7 @@ const express = require('express');
 const helmet = require("helmet")
 
 const app = express();
-app.use(helmet());
-app.disable("x-powered-by")
+app.use(helmet.hidePoweredBy());
 
 
 
