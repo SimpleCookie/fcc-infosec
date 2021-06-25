d180aa29bb9bf382fc30cad0a0b300fbfd3e00cb
Marcus <Marcus.Ekstrom@devgroup.se>
2021-06-25 19:36:01 +0200

    Add and log hash

--- server.js
+++ server.js
@@ -12,6 +12,14 @@ const someOtherPlaintextPassword = 'pass123';
 
 //START_ASYNC -do not remove notes, place code between correct pair of notes.
 
+bcrypt.hash(myPlaintextPassword, saltRounds, (err, hash) => {
+  console.log(hash)
+  /*Store hash in your db*/
+  bcrypt.compare(myPlaintextPassword, hash, (err, res) => {
+    console.log(res)
+    /*res == true or false*/
+  });
+});
 
 
 //END_ASYNC
