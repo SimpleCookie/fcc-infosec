cbae6e34b4acf1e885c8e6fc646a1b09dc65258e
Marcus <Marcus.Ekstrom@devgroup.se>
2021-06-25 18:00:50 +0200

    remove setTo

--- myApp.js
+++ myApp.js
@@ -2,7 +2,7 @@ const express = require('express');
 const helmet = require("helmet")
 
 const app = express();
-app.use(helmet.hidePoweredBy({ setTo: "foo" }))
+app.use(helmet.hidePoweredBy())
 
 
 
