595cf9af48853875db3dde849efdb229ae4f9cbc
Marcus <Marcus.Ekstrom@devgroup.se>
2021-06-29 00:38:10 +0200

    Test

--- tests/2_functional-tests.js
+++ tests/2_functional-tests.js
@@ -28,11 +28,10 @@ suite('Functional Tests', function () {
     const requester = chai.request(server).keepOpen()
     const res1 = await requester.get(url).query({ stock: "TSLA", like: true })
     const res2 = await requester.get(url).query({ stock: "TSLA", like: true })
-    console.log(res1.body)
-    console.log(res2.body)
+    console.log("res1", res1.body)
+    console.log("res2", res2.body)
     const likes1 = res1.body.stockData.likes
     const likes2 = res2.body.stockData.likes
-    console.log(likes1, ":", likes2)
     assert.strictEqual(likes1, 1)
     assert.strictEqual(likes2, 1)
     requester.close()
@@ -40,19 +39,24 @@ suite('Functional Tests', function () {
   it("Viewing two stocks", async function () {
     try {
       const res = await chai.request(server).get(url).query({ stock: ["TSLA", "GOOG"] })
-      console.log(typeof res.status, res.status)
-      assert.strictEqual(res.status, 200)
-      // assert.strictEqual(res.body.stockData.length, 2)
-      // assert.isString(res.body.stockData[0].stock)
-      // assert.isNumber(res.body.stockData[0].price)
-      // assert.isNumber(res.body.stockData[0].rel_likes)
-      // assert.strictEqual(res.body.stockData[0].stock, "TSLA")
+      console.log("body", res.body)
+      const stock1 = res.body.stockData[0]
+      const stock2 = res.body.stockData[1]
 
-      // assert.isString(res.body.stockData[1].stock)
-      // assert.isNumber(res.body.stockData[1].price)
-      // assert.isNumber(res.body.stockData[1].rel_likes)
-      // assert.strictEqual(res.body.stockData[1].stock, "GOOG")
-      // assert.strictEqual(res.body.stockData[0].rel_likes + res.body.stockData[1].rel_likes, 0)
+      assert.strictEqual(res.status, 200)
+      assert.strictEqual(res.body.stockData.length, 2)
+      assert.strictEqual(stock1.rel_likes + stock2.rel_likes, 0)
+
+      assert.isString(stock1.stock)
+      assert.isNumber(stock1.price)
+      assert.isNumber(stock1.rel_likes)
+      assert.strictEqual(stock1.stock, "TSLA")
+
+      assert.isString(stock2.stock)
+      assert.isNumber(stock2.price)
+      assert.isNumber(stock2.rel_likes)
+      assert.strictEqual(stock2.stock, "GOOG")
+
     } catch (error) {
       console.error("test", error)
     }
