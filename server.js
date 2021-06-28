e7a780d5bb7bbf7c794512df9d119c8009f62d00
Marcus <Marcus.Ekstrom@devgroup.se>
2021-06-29 00:58:24 +0200

    Add default helmet

--- server.js
+++ server.js
@@ -10,13 +10,12 @@ const fccTestingRoutes = require('./routes/fcctesting.js');
 const runner = require('./test-runner');
 
 const app = express();
-app.use(helmet({
-  contentSecurityPolicy: {
-    directives: {
-      defaultSrc: ["'self'"],
-      scriptSrc: ["'self'", "trusted-cdn.com"],
-    }
-  }
+app.use(helmet())
+app.use(helmet.contentSecurityPolicy({
+  directives: {
+    defaultSrc: ["'self'"],
+    scriptSrc: ["'self'", "trusted-cdn.com"],
+  },
 }))
 
 app.use('/public', express.static(process.cwd() + '/public'));
